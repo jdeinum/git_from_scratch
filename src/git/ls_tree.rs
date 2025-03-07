16d6f0e330b899add212baf512cd0473db2412d8
deinum <deinum@protonmail.com>
2025-03-06 19:37:54 -0700

    ls tree no test yet

--- src/git/ls_tree.rs
+++ src/git/ls_tree.rs
@@ -1,9 +1,12 @@
 use crate::git::utils::{GitObject, GitObjectType};
 use anyhow::{Context, Result, ensure};
-use std::io::{BufRead, Write};
+use std::{
+    ffi::CStr,
+    io::{BufRead, Read, Write},
+};
 pub fn run(hash: &str, name_only: bool, mut w: impl Write) -> Result<()> {
     // read in the object
-    let obj = GitObject::read(hash).context("read git object in ls_tree")?;
+    let mut obj = GitObject::read(hash).context("read git object in ls_tree")?;
     ensure!(
         obj.kind == GitObjectType::Tree,
         "ls_tree knows how to read only trees"
@@ -12,12 +15,45 @@ pub fn run(hash: &str, name_only: bool, mut w: impl Write) -> Result<()> {
     // read the entries from the buffer
     // git doesn't allow empty trees, so we don't have to worry about that case
     let mut buf: Vec<u8> = Vec::new();
+
+    // we have a separate buffer thats exactly 20 bytes for the sha
+    let mut sha_buf: [u8; 20] = [0; 20];
+
     loop {
-        // parse until null byte, which includes the mode, and the file name
-        let mode_and_filename = obj
+        // read until the null byte
+        let n = obj
             .reader
             .read_until(0, &mut buf)
             .context("read mode and filename from buffer")?;
+
+        // if we have read 0 bytes, then we're all done
+        if n == 0 {
+            break;
+        }
+
+        // convert the mode and name into a CStr
+        let mode_and_name = CStr::from_bytes_with_nul(&buf[..n])?;
+
+        // convert the CStr to a valid UTF-8 str
+        let mode_and_name = mode_and_name
+            .to_str()
+            .context("convert CStr to UTF-8 str")?;
+
+        // parse the mode and name
+        let (_mode, name) = mode_and_name
+            .split_once(' ')
+            .context("no space in tree entry")?;
+
+        // read the sha
+        obj.reader
+            .read_exact(&mut sha_buf)
+            .context("read 20 byte tree sha entry")?;
+
+        // write to the output stream
+        if name_only {
+            w.write_all(name.as_bytes())
+                .context("write name to writer")?;
+        }
     }
 
     Ok(())
